72600252dec8542dc9f135fe48a7fbc68ed606a0
tie372 <tie.372@gmail.com>
2013-08-04 11:32:06 -0600

    Add fixed timestep/variable framerate game loop;

--- main.lua
+++ main.lua
@@ -1,3 +1,37 @@
-function love.draw()
-  love.graphics.print('overgrowth', 325, 325)
+function love.load()
+  love.update = function() end
+  love.draw = function() end
+end
+
+function love.run()
+  math.randomseed(os.time())
+  math.random()
+
+  tickRate = .02
+  tickDelta = 0
+
+  love.load(arg)
+
+  while true do
+    love.timer.step()
+    local delta = love.timer.getDelta()
+
+    love.event.pump()
+    for e, a, b, c, d in love.event.poll() do
+      if e == 'quit' then if love.quit then love.quit() end love.audio.stop() return
+      else love.handlers[e](a, b, c, d) end
+    end
+
+    tickDelta = tickDelta + delta
+    while tickDelta >= tickRate do
+      tickDelta = tickDelta - tickRate
+      love.update()
+    end
+    
+    love.graphics.clear()
+    love.draw()
+    love.graphics.present()
+    
+    love.timer.sleep(.001)
+  end
 end
