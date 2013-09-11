314e1bfe4b7425b0c84b1cb8e2c0cd679f293d70
Ezra <rathmaster@gmail.com>
2013-09-11 00:20:17 -0600

    Added LoveFrames library for menu support
    Super rudimentary getup now, just so we can still enter the game. Can
    add all sorts of stuff. Very exciting library. Full documentation:
    
    http://nikolairesokav.com/documents/?page=loveframes/main

--- main.lua
+++ main.lua
@@ -6,6 +6,12 @@ Timer = require 'lib/hump/timer'
 vector = require 'lib/hump/vector'
 
 require 'lib/util'
+require 'lib/LoveFrames'
+
+require 'context/game'
+require 'context/menu'
+
+
 
 require 'class/player'
 require 'class/wall'
@@ -16,144 +22,43 @@ require 'class/view'
 
 require 'class/level'
 
-objects = {}
-history = {}
-fixtureMap = {}
+context = Menu
 
---Since planting happens during physics updates, need to queue events for later (feel free to remove if there's a better way)
-plantQueue = {}
+function contextSwitch(newcontext)
+	context.unload()
+	context = newcontext
+	context.load()
+end
 
 function love.load()
-	
-
-	love.physics.setMeter(64)
-	world = love.physics.newWorld(0, 10 * 64, true)
-	world:setCallbacks(beginCollision, endCollision, preFrameResolve, postFrameResolve)
-
-
-
-	--New way to make levels. Make as many as you want, and swap between them freely!
-	local level = Level("default")
-
-	level:addWall(0, 536, {0, 0, 764, 0, 764, 64, 0, 64})
-	level:addWall(0, 0, {0, 0, 64, 0, 64, 600, 0, 600})
-	level:addWall(700, 300, {0, 0, 64, 0, 64, 300, 0, 300})
-	level:addWall(700, 300, {0, 0, 200, 0, 200, 64, 0, 64})
-	level:addWall(900, 0, {0, 0, 64, 0, 64, 364, 0, 364})
-	level:addWall(100, 300, {0, 0, 32, 0, 32, 32, 0, 32})
-	level:setPlayer(96, 400)
-	level:addSeed(200, 400, Mushroom)
-	level:addSeed(300, 400, Bridge)
-
-	loadLevel("default")
-
-	
-end
-
-function loadLevel(levelName)
-	--Clear everything (SHOULD BE ABSTRACTED OUT)
-	fixtureMap = {}
-	objects = {}
-	history = {}
-	plantQueue = {}
-
-
-	level = levelIndex[levelName]
-	level:enter()
-
-end
-
-function love.update()
-	world:update(tickRate)
-	
-for i,seed in pairs(plantQueue) do
-    	seed.type.plant(seed.body:getX(), seed.body:getY(), seed.angle)
-    	seed:collect()
-    	table.remove(plantQueue, i)
-    end
-
-	
-	for i, obj in pairs(objects) do
-		if obj.remove then
-			table.remove(objects, i)
-		else
-			f.exe(obj.update, obj)
-		end
-	end
-
-
-	view:update()
-	
-	history[tick] = {}
-	for _, obj in pairs(objects) do
-		history[tick][obj] = {
-			x = obj.body:getX(),
-			y = obj.body:getY()
-		}
-	end
-	history[tick - 1 / tickRate] = nil
+	context.load()
 end
 
 function love.draw()
-	if not history[tick - 1] then return end
-	
-	local z = tickDelta / tickRate
-	view:draw(function()
-		for _, obj in pairs(objects) do
-			local previous, current = history[tick - 1][obj], history[tick][obj]
-			if previous and current then
-				
-				local interpolated = table.interpolate(previous, current, z)
-				local obj = table.copy(obj)
-				obj.body:setX(interpolated.x)
-				obj.body:setY(interpolated.y)
-				obj:draw()
-			end
-		end
-	end)
+	context.draw()
+end
+
+
+function love.update()
+	context.update()
 end
 
 function love.mousepressed(x, y, button)
-	--
+	context.mousepressed(x, y, button)
 end
 
 function love.mousereleased(x, y, button)
-	player:mousereleased(x, y, button)
+	context.mousereleased(x, y, button)
 end
 
 function love.keypressed(key)
-	--
+	context.keypressed(key)
 end
 
 function love.keyreleased(key)
-	player:keyreleased(key)
+	context.keyreleased(key)
 end
 
-function beginCollision(a, b, collide)
-	nX, nY = collide:getNormal()
-	a = a:getUserData()
-	b = b:getUserData()
-	f.exe(a.handleCollision, a, b, nX, nY)
-	f.exe(b.handleCollision, b, a, nX, nY)
-end
-
-function endCollision(a, b, collide)
-    
-end
-
-function preFrameResolve(a, b, collide)
-    
-end
-
-function postFrameResolve(a, b, collide)
-
-end
-
-function plantLater(seed)
-
-	plantQueue[#plantQueue + 1] = seed
-
-end
 
 function love.run()
 	math.randomseed(os.time())
