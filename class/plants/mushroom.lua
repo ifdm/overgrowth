034d8e5d557d0f1b7bdc8e328b12f1d39ac7d908
Ezra <rathmaster@gmail.com>
2013-09-10 23:42:37 -0600

    Square and Circular mushrooms
    Change a flag in mushroom.lua to switch between them. I'm thinking
    circular works better,  without a texture it's hard not to judge it on
    looks, but try to resist the temptation and give it a fair shot.

--- class/plants/mushroom.lua
+++ class/plants/mushroom.lua
@@ -1,7 +1,8 @@
 Mushroom = Class {
 	bounceSpeed = -5,
 	maxBounceVelocity = -3500,
-	name = "Mushroom"
+	name = "Mushroom",
+	curvedMushrooms = false
 }
 
 function Mushroom.plant(x, y, angle)
@@ -18,7 +19,11 @@ function Mushroom:init(x, y, angle)
 	self.body = _body
 	self.body:setAngle(angle)
 	self.body:setMass(15)
-	self.shape = love.physics.newPolygonShape(0, 0, 150, 0, 150, 64, 0, 64)
+	if self.curvedMushrooms == true then
+		self.shape = love.physics.newCircleShape(60)
+	else
+		self.shape = love.physics.newPolygonShape(0, 0, 150, 0, 150, 64, 0, 64)
+	end
 	self.fixture = love.physics.newFixture(self.body, self.shape, 1)
 	self.fixture:setUserData(self)
 
@@ -44,5 +49,11 @@ end
 
 function Mushroom:draw()
 	love.graphics.reset()
-	love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+	if self.curvedMushrooms == true then
+		love.graphics.circle('fill', self.body:getX(), self.body:getY(), self.shape:getRadius())
+	else
+		love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
+	end
+	
+	
 end
