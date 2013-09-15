4035f37ffdcc174244b63a601c69b6b74be1e156
Ezra <rathmaster@gmail.com>
2013-09-15 10:47:00 -0600

    Fan now has blocking behavior
    Added heap.lua for priority queues. I'm using them for raycasts to
    quickly get the closest object to be hit by a ray.
    
    If you spam test seeds it will cause some of the force to get through,
    but the player won't be spamming test seeds.
    
    
    --Hotfix - removed spam print messages, fan kills player

--- class/obstacles/fan.lua
+++ class/obstacles/fan.lua
@@ -13,7 +13,7 @@ function Fan:init(x, y, angle, force)
 	--CREATE BODY
 	local _body = love.physics.newBody(world, x, y, "static")
 	self.body = _body
-	--self.body:setAngle(angle)
+	self.body:setAngle(angle)
 	self.body:setMass(15)
 	self.shape = love.physics.newPolygonShape(0, 0, 64, 0, 64, 128, 0, 128)
 	self.fixture = love.physics.newFixture(self.body, self.shape, 1)
@@ -27,22 +27,40 @@ function Fan:init(x, y, angle, force)
 
 	self.force = force
 	self.lines = {}
-	self.targetX = x + 32
+	self.curHeap = {}
 
-	local x0 = self.targetX - self.radius
-	local x1 = self.targetX + self.radius
+	self.unitVector = vector(math.cos(angle), math.sin(angle)):normalize_inplace()
+	self.orthVector = vector(math.cos(angle + math.pi/2), math.sin(angle + math.pi/2))
+	self.targetX = x + (self.unitVector.x * 32) + (self.unitVector.y * 64)
+	self.targetY = y + (self.unitVector.x * 32) + (self.unitVector.y * 64)
 
-	for i=y, y+128, 8 do
+	print("angle "..angle)
+	print(self.unitVector)
+
+
+	for i=0, 128, 8 do
+		local cX = x + self.orthVector.x * i
+		local cY = y + self.orthVector.y * i
+
+		self.offsetVector = vector(cX, cY)
+		local startPoint = (self.offsetVector + self.unitVector * self.radius):clone()
+		local endPoint = (self.offsetVector + self.unitVector * (self.radius * -1)):clone()
 		self.lines[#self.lines + 1] = {
-			x0 = x0,
-			y0 = i,
-			x1 = x1,
-			y1 = i
+			x0 = cX,
+			y0 = cY,
+			x1 = startPoint.x,
+			y1 = startPoint.y
+			
 		}
+		self.lines[#self.lines + 1] = {
+			x0 = cX,
+			y0 = cY,
+			x1 = endPoint.x,
+			y1 = endPoint.y
+		}
+		
 
 	end
-
-
 
 	self.activated = true
 
@@ -52,12 +70,21 @@ end
 function FanRayCastCallback(fixture, x, y, xn, yn, fraction)
 	if not fixture:getUserData().name then return -1 end
 	if not (fixture:getBody():getType() == "dynamic") then return -1 end
-	if Fan.donothit[fixture] then return -1 end
+	--if Fan.donothit[fixture] then return -1 end
 
-	Fan.donothit[fixture] = 1
-	local dist = (x - Fan.fanForRaycast.targetX)
-	local body = fixture:getBody()
-	body:applyForce((Fan.radius - dist) * Fan.fanForRaycast.force, 0)
+	--Fan.donothit[fixture] = 1
+	Fan.fanForRaycast.lineEnds[Fan.fanForRaycast.curLine] = {
+		x0 = nil,
+		y0 = nil,
+		x1 = x,
+		y1 = y
+	}
+	local dist = (x - Fan.fanForRaycast.targetX)^2 + (y - Fan.fanForRaycast.targetY)^2
+	Fan.fanForRaycast.curHeap:push(fixture, dist)
+				print("dist ".. dist)
+
+	--local body = fixture:getBody()
+	--body:applyForce((Fan.radius - dist) * Fan.fanForRaycast.force, 0)
 	
 	--if(fixture:getUserData().name == "Player") then 
 		--print("Fan hit something "..fixture:getUserData().name .. " x "..x .. ", y".. y .. ", dist " .. dist .. ", force " .. ((1000 - dist) * 0.35))
@@ -69,10 +96,33 @@ end
 
 function Fan:update()
 	Fan.donothit = {}
+	self.lineEnds = {}
 	Fan.fanForRaycast = self
-	for _, line in pairs(self.lines) do
+	for i, line in pairs(self.lines) do
+		self.curHeap = Heap()
+		self.curLine = i
 		world:rayCast(line.x0, line.y0, line.x1, line.y1, FanRayCastCallback)
-
+		if not self.lineEnds[i] then
+			self.lineEnds[i] = {
+				x0 = nil,
+				y0 = nil,
+				x1 = line.x1,
+				y1 = line.y1
+			}
+		end
+		self.lineEnds[i].x0 = line.x0
+		self.lineEnds[i].y0 = line.y0
+		if not self.curHeap:isempty() then
+			local fixture, dist = self.curHeap:pop()
+			if fixture then
+				local body = fixture:getBody()
+				if body then
+					local force = (self.radius - dist) * self.force
+					print("Force " .. force)
+					body:applyForce(force * self.unitVector.x, force * self.unitVector.y)
+				end
+			end
+		end
 	end
 
 end
@@ -95,8 +145,9 @@ function Fan:draw()
 	love.graphics.setColor(120, 120, 120)
 	love.graphics.polygon('fill', self.body:getWorldPoints(self.shape:getPoints()))
 
-	for _, line in pairs(self.lines) do
+	for _, line in pairs(self.lineEnds) do
 		love.graphics.line(line.x0, line.y0, line.x1, line.y1)
+		love.graphics.circle("fill", line.x1, line.y1, 2)
 
 	end
 end
