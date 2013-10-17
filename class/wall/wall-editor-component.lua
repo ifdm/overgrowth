8301176f33296421d22f01dbdee9ac7eeeb0e584
tie372 <tie.372@gmail.com>
2013-10-16 19:58:32 -0600

    Organize wall-editor-component;

--- class/wall/wall-editor-component.lua
+++ class/wall/wall-editor-component.lua
@@ -2,25 +2,10 @@ WallEditorComponent = {
 	name = 'WallEditorComponent'
 }
 
+----------------
+-- Core
+----------------
 function WallEditorComponent:init()
-	self.selected = false
-end
-
-function WallEditorComponent:move(delta)
-	if self.selected then
-		self.body:setX(self.body:getX() + delta.x)
-		self.body:setY(self.body:getY() + delta.y)		
-	end
-end
-
-function WallEditorComponent:select(x, y)
-	if math.inside(x, y, self:getBoundingBox()) then
-		self.selected = true
-		return self
-	end
-end
-
-function WallEditorComponent:deselect()
 	self.selected = false
 end
 
@@ -31,4 +16,28 @@ function WallEditorComponent:draw()
 		love.graphics.setColor(0, 255, 255, 255)
 		love.graphics.rectangle('line', self:getBoundingBox())
 	end
+end
+
+----------------
+-- Move
+----------------
+function WallEditorComponent:move(delta)
+	if self.selected then
+		self.body:setX(self.body:getX() + delta.x)
+		self.body:setY(self.body:getY() + delta.y)		
+	end
+end
+
+----------------
+-- Select
+----------------
+function WallEditorComponent:select(x, y)
+	if math.inside(x, y, self:getBoundingBox()) then
+		self.selected = true
+		return self
+	end
+end
+
+function WallEditorComponent:deselect()
+	self.selected = false
 end
